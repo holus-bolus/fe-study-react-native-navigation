1e5f6e15f02da7d1264fc2ab0c7b2ee9f0cb6a97
Oleksii Ustinov <93076661+holus-bolus@users.noreply.github.com>
2024-06-25 11:29:03 +0300

    Refactored the code to output items

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,34 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, FlatList } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
-const MealsOverviewScreen = () => {
+/**
+ *
+ * @param route
+ * @returns {JSX.Element}
+ * @constructor
+ */
+
+const MealsOverviewScreen = ({ route }) => {
+  // const route = useRoute()
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0,
+  );
+
+  const renderMealItem = (itemData) => {
+    return <MealItem title={itemData.item.title} />;
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview screen</Text>
-      <Text>World</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 };
